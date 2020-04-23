9c14b359b6ea8ea3a5ed17a177876ffd8b652e94
naveenezone <naveen.paulraj@n4softsol.ca>
2020-04-23 12:56:53 -0400

    CompletedBasicAuthv1.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ITEMNG'`, () => {
+  it(`should have as title 'samh2dsecbas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ITEMNG');
+    expect(app.title).toEqual('samh2dsecbas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ITEMNG app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('samh2dsecbas app is running!');
   });
 });
